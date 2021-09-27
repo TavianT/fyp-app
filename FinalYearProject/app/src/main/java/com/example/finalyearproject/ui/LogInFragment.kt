2482ddd53c2a056056412f3aa0ed188fc77d5676
Tavian <taviantaylor00@gmail.com>
2021-09-27 23:05:33 +0100

    Google sign in functionality started

--- FinalYearProject/app/src/main/java/com/example/finalyearproject/ui/LogInFragment.kt
+++ FinalYearProject/app/src/main/java/com/example/finalyearproject/ui/LogInFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.finalyearproject.R
 import com.example.finalyearproject.databinding.FragmentFlowerFinderBinding
 import com.example.finalyearproject.databinding.FragmentLogInBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 
 
 class LogInFragment : Fragment() {
     private var _binding: FragmentLogInBinding? = null
     private val binding get() = _binding!!
+    val gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestIdToken("889641631435-7bnmkth5qi6sk73t72u05mejfjj7d72g.apps.googleusercontent.com")
+        .requestEmail()
+        .build()
+    val googleSignInClient: GoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +36,40 @@ class LogInFragment : Fragment() {
 
         val root: View = binding.root
         binding.googleSignInButton.setSize(SignInButton.SIZE_STANDARD)
+        binding.googleSignInButton.setOnClickListener {
+
+
+        }
+        binding.textViewRegister.setOnClickListener{
+            root.findNavController().navigate(R.id.action_logInFragment_to_registerFragment)
+        }
+
         return root
     }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+//    private fun signIn() {
+//        val signInIntent = googleSignInClient.signInIntent
+//        startActivityForResult(signInIntent, RC_SIGN_IN)
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+//        if (requestCode == RC_SIGN_IN) {
+//            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+//            try {
+//                // Google Sign In was successful, authenticate with Firebase
+//                val account = task.getResult(ApiException::class.java)!!
+//                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+//                firebaseAuthWithGoogle(account.idToken!!)
+//            } catch (e: ApiException) {
+//                // Google Sign In failed, update UI appropriately
+//                Log.w(TAG, "Google sign in failed", e)
+//            }
+//        }
+//    }
 }
